242632fde2c4ae0380ad83612f913822439aab21
profe29 <56177992+profe29@users.noreply.github.com>
2019-10-07 17:22:51 -0300

    Update Create.cs

--- Create.cs
+++ Create.cs
@@ -7,36 +7,39 @@ del segundo campo (columna) y los registros
 filtrados se guardan en otro archivo de texto
 separado por comas.
 */
+using System;
+using System.IO;
+using System.Collections.Generic;
 
 class Create
 {
     static void Main(string[] args)
-    {		
-		String[] text = File.ReadAllLines("C:\\users\\alumno\\ver.txt");
+    {
+        String[] text = File.ReadAllLines("C:\\Users\\Reconquista\\ver.txt");
 
-		Stack<string> resultado = new Stack<string>();
-		String producto;
-		int cantidad;
-		String estado;
+        Stack<string> resultado = new Stack<string>();
+        String producto;
+        int cantidad;
+        String estado;
 
-		resultado.Push("Procesado ok " + DateTime.Today);
+        resultado.Push("Procesado ok " + DateTime.Today);
 
-		foreach(var line in text)
-		{
-			var campos = line.Split(',');
-			producto = campos[0];
-			cantidad = Int32.Parse(campos[1]);
-			estado = campos[2];
-			
-			if(cantidad < -20)
-			{
-			   resultado.Push(line);
-			}
-			
-		}
-		String[] arr = resultado.ToArray(); 
+        foreach (var line in text)
+        {
+            var campos = line.Split(',');
+            producto = campos[0];
+            cantidad = Int32.Parse(campos[1]);
+            estado = campos[2];
 
-		System.IO.File.WriteAllLines("C:\\users\\alumno\\ver.txt", arr.ToArray());	
-	}
+            if (cantidad < 15)
+            {
+                resultado.Push(line);
+            }
+
+        }
+        String[] arr = resultado.ToArray();
+
+        System.IO.File.WriteAllLines("C:\\Users\\Reconquista\\out.txt", arr);
+    }
 }
 
